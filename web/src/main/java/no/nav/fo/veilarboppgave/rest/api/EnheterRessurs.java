9c1db983e7abb2e27090c50eb3c1b7e371a28012
Peder Korsveien <peder.korsveien@nav.no>
2017-09-13 13:44:54 +0200

    Legg til validering på enhetsressurs

--- web/src/main/java/no/nav/fo/veilarboppgave/rest/api/EnheterRessurs.java
+++ web/src/main/java/no/nav/fo/veilarboppgave/rest/api/EnheterRessurs.java
@@ -10,22 +10,30 @@ import java.util.Arrays;
 import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static no.nav.fo.veilarboppgave.rest.api.Validering.valider;
 
 @Path("/enheter/")
 @Produces(APPLICATION_JSON)
 @Consumes(APPLICATION_JSON)
 public class EnheterRessurs {
 
-    @Inject
-    ArbeidsfordelingService arbeidsfordelingService;
+    private final ArbeidsfordelingService arbeidsfordelingService;
+    private final PersonService personService;
 
     @Inject
-    PersonService personService;
+    public EnheterRessurs(ArbeidsfordelingService arbeidsfordelingService, PersonService personService) {
+        this.arbeidsfordelingService = arbeidsfordelingService;
+        this.personService = personService;
+    }
 
     @GET
     @Path("{fnr}/")
     public List<Enhet> hentEnheter(@PathParam("fnr") String fnr) {
-        //TODO: Sjekk tilgang!!
+
+        valider(fnr)
+                .map(Validering::erGyldigFnr)
+                .map(Validering::sjekkTilgangTilBruker);
+
         return Arrays.asList(
                 Enhet.of("0106", "NAV Fredrikstad"),
                 Enhet.of("0105", "NAV Sarpsborg"),
---
+++ web/src/main/java/no/nav/fo/veilarboppgave/rest/api/Validering.java
@@ -0,0 +1,32 @@
+package no.nav.fo.veilarboppgave.rest.api;
+
+import no.nav.apiapp.feil.UgyldigRequest;
+import no.nav.apiapp.security.PepClient;
+
+import javax.inject.Inject;
+import java.util.Optional;
+
+import static java.util.Optional.ofNullable;
+import static no.bekk.bekkopen.person.FodselsnummerValidator.isValid;
+
+public class Validering {
+
+    @Inject
+    private static PepClient pepClient;
+
+    public static Optional<String> valider(String data) {
+        return ofNullable(data);
+    }
+
+    public static String sjekkTilgangTilBruker(String fnr) {
+        pepClient.sjekkTilgangTilFnr(fnr);
+        return fnr;
+    }
+
+    public static String erGyldigFnr(String fnr) {
+        if (isValid(fnr)) {
+            return fnr;
+        }
+        throw new UgyldigRequest();
+    }
+}
