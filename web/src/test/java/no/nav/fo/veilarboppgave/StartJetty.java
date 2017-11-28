ed858b79327499aa51df0f24743537e85a3a6178
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-11-28 19:57:23 +0100

    fikset bygg ved å forenkle jetty-oppsettet

--- web/src/test/java/no/nav/fo/veilarboppgave/StartJetty.java
+++ web/src/test/java/no/nav/fo/veilarboppgave/StartJetty.java
@@ -7,11 +7,14 @@ import no.nav.dialogarena.config.fasit.FasitUtils;
 import no.nav.fo.veilarboppgave.config.DatabaseConfig;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import org.eclipse.jetty.plus.jndi.Resource;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
+import org.eclipse.jetty.webapp.WebAppContext;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 import java.io.File;
 
 import static java.lang.System.getProperty;
+import static java.util.Arrays.stream;
 import static no.nav.dialogarena.config.DevelopmentSecurity.setupISSO;
 import static no.nav.fo.veilarboppgave.config.LocalJndiContextConfig.setupInMemoryDatabase;
 import static no.nav.fo.veilarboppgave.config.LocalJndiContextConfig.setupOracleDataSource;
@@ -61,13 +64,21 @@ public class StartJetty {
     }
 
     public static Jetty startJettyUtenSikkerhet() {
-        return Jetty
+        Jetty jetty = Jetty
                 .usingWar()
                 .at(APPLICATION_NAME)
                 .port(PORT)
                 .overrideWebXml(new File("src/test/resources/disable-security-web.xml"))
                 .disableAnnotationScanning()
-                .buildJetty()
-                .start();
+                .buildJetty();
+
+        // MetaInfConfiguration førte til "java.util.zip.ZipException: error in opening zip file"
+        WebAppContext context = jetty.context;
+        String[] configurations = stream(context.getConfigurationClasses())
+                .filter(className -> !MetaInfConfiguration.class.getName().equals(className))
+                .toArray(String[]::new);
+        context.setConfigurationClasses(configurations);
+
+        return jetty.start();
     }
 }
