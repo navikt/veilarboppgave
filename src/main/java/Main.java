9e6b185e69acbbc1cb131b85f0d76907b7180ffc
Stephen Ramthun <stephen.ramthun@nav.no>
2018-09-05 15:20:41 +0200

    PUS-14 Oppdater aktoer endpoint url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,13 @@ import no.nav.apiapp.ApiApp;
 import no.nav.fo.veilarboppgave.config.ApplicationConfig;
 
 import static no.nav.fo.veilarboppgave.config.ApplicationConfig.AKTOER_V2_ENDPOINTURL;
+import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.setProperty(AKTOER_V2_ENDPOINTURL, getRequiredProperty(AKTOER_V2_ENDPOINTURL));
+        System.setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty(AKTOER_V2_ENDPOINTURL));
 
         ApiApp.runApp(ApplicationConfig.class, args);
     }
