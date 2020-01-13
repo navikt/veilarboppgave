74519a81a4ff025bd63b60e64098e4f9f5854e2a
Peder Korsveien <peder.korsveien@nav.no>
2020-01-13 13:42:25 +0100

    KZ-0 Les inn creds og fiks feil i gha-config

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 import no.nav.apiapp.ApiApp;
+import no.nav.brukerdialog.tools.SecurityConstants;
+import no.nav.common.utils.NaisUtils;
 import no.nav.fo.veilarboppgave.config.ApplicationConfig;
+import no.nav.sbl.dialogarena.common.abac.pep.CredentialConstants;
+import no.nav.sbl.dialogarena.common.cxf.StsSecurityConstants;
 
 import static java.lang.System.setProperty;
+import static no.nav.common.utils.NaisUtils.getCredentials;
 import static no.nav.fo.veilarboppgave.config.ApplicationConfig.AKTOER_V2_ENDPOINTURL;
 import static no.nav.dialogarena.aktor.AktorConfig.AKTOER_ENDPOINT_URL;
 import static no.nav.sbl.featuretoggle.unleash.UnleashServiceConfig.UNLEASH_API_URL_PROPERTY_NAME;
@@ -10,6 +15,25 @@ import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 public class Main {
 
     public static void main(String[] args) {
+
+        NaisUtils.Credentials serviceUser = NaisUtils.getCredentials("service_user");
+
+        //ABAC
+        System.setProperty(CredentialConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(CredentialConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //CXF
+        System.setProperty(StsSecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(StsSecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        //OIDC
+        System.setProperty(SecurityConstants.SYSTEMUSER_USERNAME, serviceUser.username);
+        System.setProperty(SecurityConstants.SYSTEMUSER_PASSWORD, serviceUser.password);
+
+        NaisUtils.Credentials oracleCreds = getCredentials("oracle_creds");
+        System.setProperty("VEILARBOPPGAVEDB_USERNAME", oracleCreds.username);
+        System.setProperty("VEILARBOPPPGAVEDB_PASSWORD", oracleCreds.password);
+
         System.setProperty(AKTOER_ENDPOINT_URL, getRequiredProperty(AKTOER_V2_ENDPOINTURL));
         setProperty(UNLEASH_API_URL_PROPERTY_NAME, "https://unleash.nais.adeo.no/api/");
 
