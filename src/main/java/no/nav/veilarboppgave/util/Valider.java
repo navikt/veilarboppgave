928b0eceff579f691b401b23e03990984d2b50fb
Alexander Gård <alex.gaard@hotmail.com>
2020-10-29 12:52:50 +0100

    Liten refaktorering av validering av oppgave og gjorde avsenderenhetId til ikke påkrevd

--- src/main/java/no/nav/veilarboppgave/util/Valider.java
+++ src/main/java/no/nav/veilarboppgave/util/Valider.java
@@ -1,22 +1,20 @@
 package no.nav.veilarboppgave.util;
 
-import no.nav.common.utils.StringUtils;
-import no.nav.veilarboppgave.domain.OppgaveType;
-import no.nav.veilarboppgave.domain.Prioritet;
-import no.nav.veilarboppgave.domain.TemaDTO;
-import no.nav.veilarboppgave.domain.OppgaveDTO;
+import no.nav.veilarboppgave.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 
-import static java.util.Optional.ofNullable;
-
 public class Valider {
 
     public static void validerOppgaveDto(OppgaveDTO oppgaveDto) {
-        validerAtFeltErUtfylt(oppgaveDto.getAvsenderenhetId());
-        validerAtFeltErUtfylt(oppgaveDto.getEnhetId());
+        // Avsenderenhet er ikke påkrevd, men må være gyldig hvis det blir sendt med
+        if (oppgaveDto.getAvsenderenhetId() != null) {
+            validerEnhetId(oppgaveDto.getAvsenderenhetId());
+        }
+
+        validerEnhetId(oppgaveDto.getEnhetId());
 
         validerTema(oppgaveDto.getTema());
         validerPrioritet(oppgaveDto.getPrioritet());
@@ -28,27 +26,33 @@ public class Valider {
     }
 
     public static void validerTema(String tema) {
-        ofNullable(tema)
-                .map(Valider::validerAtFeltErUtfylt)
-                .map(String::toUpperCase)
-                .filter(TemaDTO::contains)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema er ugyldig"));
+        if (tema == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema mangler");
+        }
+
+        if (!TemaDTO.contains(tema.toUpperCase())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema er ugyldig: " + tema);
+        }
     }
 
     private static void validerPrioritet(String prioritet) {
-        ofNullable(prioritet)
-                .map(Valider::validerAtFeltErUtfylt)
-                .map(String::toUpperCase)
-                .filter(Prioritet::contains)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Prioritet er ugyldig"));
+        if (prioritet == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Prioritet mangler");
+        }
+
+        if (!Prioritet.contains(prioritet.toUpperCase())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Prioritet er ugyldig: " + prioritet);
+        }
     }
 
     public static void validerOppgaveType(String oppgaveType) {
-         ofNullable(oppgaveType)
-                .map(Valider::validerAtFeltErUtfylt)
-                .map(String::toUpperCase)
-                .filter(OppgaveType::contains)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Oppgavetype er ugyldig"));
+        if (oppgaveType == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Oppgavetype mangler");
+        }
+
+        if (!OppgaveType.contains(oppgaveType.toUpperCase())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Oppgavetype er ugyldig: " + oppgaveType);
+        }
     }
 
     public static void validerFraDatoErForTilDato(String fraDato, String tilDato) {
@@ -61,21 +65,20 @@ public class Valider {
         }
     }
 
-    public static String validerAtFeltErUtfylt(String felt) {
-        return StringUtils.of(felt).orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Felt er ikke utfylt"));
+    public static void validerEnhetId(String enhetId) {
+        if (enhetId == null || enhetId.length() != 4) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Enhet ID er ugyldig: " + enhetId);
+        }
     }
 
     public static void validerBeskrivelse(String beskrivelse) {
-        ofNullable(beskrivelse)
-                .map(Valider::validerAtFeltErUtfylt)
-                .map(Valider::erIkkeOver250Tegn)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Beskrivelse er ugyldig"));
-    }
+        if (beskrivelse == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Beskrivelse mangler");
+        }
 
-    private static String erIkkeOver250Tegn(String beskrivelse) {
         if (beskrivelse.length() > 250) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Beskrivelse må være 250 tegn eller mindre");
         }
-        return beskrivelse;
     }
+
 }
