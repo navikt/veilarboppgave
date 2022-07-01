ea6c7df5090d0a1920b50c797fefcadb75cab477
stianStensli <stian.horn.stensli@nav.no>
2022-07-01 12:31:57 +0200

    bruker metoden: getUid

--- src/main/java/no/nav/veilarboppgave/service/AuthService.java
+++ src/main/java/no/nav/veilarboppgave/service/AuthService.java
@@ -40,13 +40,8 @@ public class AuthService {
 
     // NAV ident, fnr eller annen ID
     public String getInnloggetBrukerIdent() {
-        return AuthContextHolderThreadLocal
-                .instance().getNavIdent()
-                .map(NavIdent::toString)
-                .orElse(authContextHolder
-                                .getSubject()
-                                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Id is missing from subject"))
-                );
-    }
+        return AuthContextHolderThreadLocal.instance()
+                .getUid()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Id is missing from subject"));
 
 }
