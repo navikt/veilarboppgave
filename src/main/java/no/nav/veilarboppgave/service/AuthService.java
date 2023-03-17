91cb1fd638c17775da5753ea87dd7bcee7eb2dd2
Oda Dahlen <oda.dahlen@nav.no>
2023-03-17 11:15:33 +0100

    Lagt til det nødvendige for å bruke featuretoggle i repoet samt definert featuretoggle for poao-tilgang

--- src/main/java/no/nav/veilarboppgave/service/AuthService.java
+++ src/main/java/no/nav/veilarboppgave/service/AuthService.java
@@ -1,7 +1,9 @@
 package no.nav.veilarboppgave.service;
 
+import com.nimbusds.jwt.JWTClaimsSet;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import no.finn.unleash.FeatureToggle;
 import no.nav.common.abac.Pep;
 import no.nav.common.abac.domain.request.ActionId;
 import no.nav.common.auth.context.AuthContextHolder;
@@ -10,9 +12,20 @@ import no.nav.common.client.aktoroppslag.AktorOppslagClient;
 import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.Fnr;
 import no.nav.common.types.identer.NavIdent;
+import no.nav.common.utils.AuthUtils;
+import no.nav.poao_tilgang.client.Decision;
+import no.nav.poao_tilgang.client.NavAnsattTilgangTilEksternBrukerPolicyInput;
+import no.nav.poao_tilgang.client.PoaoTilgangClient;
+import no.nav.poao_tilgang.client.TilgangType;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.ofNullable;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,16 +36,33 @@ public class AuthService {
 
     private final AktorOppslagClient aktorOppslagClient;
     private final AuthContextHolder authContextHolder;
+    private final PoaoTilgangClient poaoTilgangClient;
+    private final UnleashService unleashService;
+
+    public Decision harTilgangTilPerson(Fnr fnr) {
+        return poaoTilgangClient.evaluatePolicy(new NavAnsattTilgangTilEksternBrukerPolicyInput(
+                hentInnloggetVeilederUUID(authContextHolder),
+                TilgangType.LESE,
+                fnr.get())
+        ).getOrThrow();
+    }
 
     public void sjekkLesetilgangMedAktorId(AktorId aktorId) {
-//
-        if (!veilarbPep.harTilgangTilPerson(getInnloggetBrukerToken(), ActionId.READ, aktorId)) {
-            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        if (unleashService.isPoaoTilgangEnabled()) {
+            harTilgangTilPerson(getFnrOrThrow(aktorId));
+        } else {
+            if (!veilarbPep.harTilgangTilPerson(getInnloggetBrukerToken(), ActionId.READ, aktorId)) {
+                throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+            }
         }
     }
 
     public AktorId getAktorIdOrThrow(Fnr fnr) {
         return aktorOppslagClient.hentAktorId(fnr);
+    }
+
+    public Fnr getFnrOrThrow(AktorId aktorId){
+        return aktorOppslagClient.hentFnr(aktorId);
     }
 
     public String getInnloggetBrukerToken() {
@@ -45,4 +75,20 @@ public class AuthService {
                 .getUid()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Id is missing from subject"));
     }
+
+    public static UUID hentInnloggetVeilederUUID(AuthContextHolder authContextHolder) {
+        return authContextHolder.getIdTokenClaims()
+                .flatMap(claims -> getStringClaimOrEmpty(claims, "oid"))
+                .map(UUID::fromString)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.FORBIDDEN, "Fant ikke oid for innlogget veileder"));
+    }
+
+    public static Optional<String> getStringClaimOrEmpty(JWTClaimsSet claims, String claimName) {
+        try {
+            return ofNullable(claims.getStringClaim(claimName));
+        } catch (Exception e) {
+            return empty();
+        }
+    }
+
 }
