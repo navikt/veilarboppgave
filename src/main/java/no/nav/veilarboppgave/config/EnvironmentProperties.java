7447c0f676c5c9ac3f1f4096f79fe736b1b9def6
Dragutin Vujovic <66312566+dragutin-nav@users.noreply.github.com>
2024-07-15 18:39:46 +0200

    Move to gcp (#167)
    * Move to GCP

--- src/main/java/no/nav/veilarboppgave/config/EnvironmentProperties.java
+++ src/main/java/no/nav/veilarboppgave/config/EnvironmentProperties.java
@@ -9,28 +9,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "app.env")
 public class EnvironmentProperties {
 
-    private String openAmDiscoveryUrl;
-
-    private String veilarbloginOpenAmClientId;
-
-    private String openAmRefreshUrl;
-
     private String naisAadDiscoveryUrl;
 
     private String naisAadClientId;
 
     public String naisAadIssuer;
 
-    private String naisStsDiscoveryUrl;
-
-    private String naisStsClientId;
-
-
     private String norg2Url;
-
-    private String aktorregisterUrl;
-
-    private String soapStsUrl;
 
     private String dbUrl;
 
